9814da91e35da875dc1c39d2ab353596ca1b43c7
BHARATHARSEN <bcgrkofficial@gmail.com>
2024-08-01 21:02:08 -0500

    Modified connection to have Dynamic switch between databases

--- config.js
+++ config.js
@@ -1,15 +1,34 @@
 import mysql from 'mysql2';
+import dotenv from "dotenv";
+import { fileURLToPath } from "url";
+import path from "path";
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: "root",
-    password: "",
-    database: "",
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-});
+// Loading environment variables from .env file
+dotenv.config({ path: path.resolve(__dirname, ".env") });
+
+const connection = {
+  database1: mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: database1,
+  }),
+  database2: mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: database2,
+  }),
+};
 
 connection.connect((err) => {
-    if(err) throw err;
+    if(err) {
+        console.error("Error connecting to the database:", err.stack);
+        return;
+    };
     console.log("Connected to the MySQL server.");
 });
 
