44eba0ab3a2cd22cedfc0472c25369e871e95a91
BHARATHARSEN <bcgrkofficial@gmail.com>
2024-08-07 15:07:16 -0500

    Added Token creator function

---
+++ backend/config/token.js
@@ -0,0 +1,6 @@
+import crypto from "crypto"
+
+// Generate a random 32-byte (256-bit) buffer
+const secret = crypto.randomBytes(32);
+
+console.log(secret.toString("hex")); // Output: 64 characters long hexadecimal string
