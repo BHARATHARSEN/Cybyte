56bf91bafa9b00dd98bde18c4bea7d7eec6134f9
BHARATHARSEN <bcgrkofficial@gmail.com>
2024-08-11 13:51:06 -0500

    Added new dummy route to change the database based on query

---
+++ backend/src/routes/dummyRoute.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import { handleRequest } from './dummy';
+
+const router = express.Router();
+
+// Define types for request and response objects
+import { Request, Response } from 'express';
+
+// Type definition for route handler
+router.get('/:id', (req: Request, res: Response) => handleRequest(req, res));
+
+export default router;
