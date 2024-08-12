aef420c8edd94c71131d367703172264214626dc
BHARATHARSEN <bcgrkofficial@gmail.com>
2024-08-12 12:24:31 -0500

    Added comments

--- backend/src/config/token.ts
+++ backend/src/config/token.ts
@@ -1,14 +1,7 @@
-// import crypto from "crypto"
-
-// // Generate a random 32-byte (256-bit) buffer
-// const secret = crypto.randomBytes(32);
-
-// console.log(secret.toString("hex")); // Output: 64 characters long hexadecimal string
-
 import crypto from 'crypto';
 
-// Generate a random 32-byte (256-bit) buffer
+// Generating a random 32-byte (256-bit) buffer
 const secret: Buffer = crypto.randomBytes(32);
 
-console.log(secret.toString('hex')); // Output: 64 characters long hexadecimal string
+console.log(secret.toString('hex')); // 64 characters long hexadecimal string
 
