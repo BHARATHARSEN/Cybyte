70347a025c8c2855e355c6ba8cf234206a1386a6
BHARATHARSEN <bcgrkofficial@gmail.com>
2024-08-20 19:38:14 -0500

    Added this function to check if the request consists of a token in headers

--- frontend/src/authGuard.ts
+++ frontend/src/authGuard.ts
@@ -2,22 +2,22 @@ import { inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { CanActivateFn } from '@angular/router';
 
-export const authGuard: CanActivateFn = (route, state) => {
+export const authGuard: CanActivateFn = () => {
   const router = inject(Router);
   
-  // Check if the code is running in the browser
+  // Checking if the code is running in the browser
   if (typeof window !== 'undefined') {
     const token = localStorage.getItem('token');
 
     if (token) {
-      // Token exists, allow access
+      
       return true;
     } else {
-      // No token, redirect to login
+      
       return router.parseUrl('/login');
     }
   } else {
-    // Server-side rendering or non-browser environment
+    // SSR or non-browser environment
     return router.parseUrl('/login');
   }
 };
