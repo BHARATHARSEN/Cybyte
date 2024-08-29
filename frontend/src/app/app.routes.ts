73d04a41c5e096f7d00d6cb2e578afd6592eae86
BHARATHARSEN <bcgrkofficial@gmail.com>
2024-08-29 13:15:45 -0500

    Added Form view component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { FormPageComponent } from '../form-page/form-page.component';
 import { LoginPageComponent } from '../login-page/login-page.component';
 import { authGuard } from '../authGuard';
 import { SignUpPageComponent } from '../sign-page/sign-page.component';
+import { FormViewComponent } from '../form-view/form-view.component';
 
 export const routes: Routes = [
     { path : '', redirectTo: '/login', pathMatch:'full'},
     { path: 'login', component: LoginPageComponent },
     { path: 'signup', component: SignUpPageComponent },
     { path: 'form', component: FormPageComponent, canActivate: [authGuard] },
+    { path: 'forms', component: FormViewComponent, canActivate: [authGuard]}
     
 ];
