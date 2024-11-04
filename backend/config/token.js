12b0e244054405d8a79bf57bacc3be23a16048d8
BHARATHARSEN <bcgrkofficial@gmail.com>
2024-11-03 20:12:37 -0600

    Uploading all the files

--- backend/config/token.js
+++
@@ -1,6 +0,0 @@
-import crypto from "crypto"
-
-// Generate a random 32-byte (256-bit) buffer
-const secret = crypto.randomBytes(32);
-
-console.log(secret.toString("hex")); // Output: 64 characters long hexadecimal string
